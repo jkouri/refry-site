127a48eff019c8c060ccac413ae3f274c4fa6b72
Peleg Rosenthal <peleg@teacherspayteachers.com>
2016-07-27 11:11:33 -0400

    optimized prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,56 @@
+'use strict';
+
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ComponentResolverPlugin = require('component-resolver-webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const env = process.env.NODE_ENV || 'development';
+const isProduction = env === 'production';
+
+const entryPoints = [
+  path.join(__dirname, 'src', 'bootstrap'),
+  path.join(__dirname, 'src', 'style.scss')
+];
+
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(env)
+  }),
+  new HtmlWebpackPlugin({
+    inject: true,
+    template: path.join(__dirname, 'src', 'index.html')
+  }),
+  new ExtractTextPlugin({ filename: 'style.css', allChunks: true }),
+  new webpack.optimize.CommonsChunkPlugin('vendor')
+];
+
+let devtool;
+
+if (isProduction) {
+  devtool = 'cheap-module-source-map';
+  plugins.push(
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: true },
+      sourceMap: false
+    })
+  );
+} else {
+  devtool = false;
+  entryPoints.unshift('webpack-dev-server/client?http://localhost:8080');
+}
+
 module.exports = {
-  cache: true,
-  watch: true,
-  devtool: 'source-map',
+  devtool,
   entry: {
-    main: [
-      'webpack-dev-server/client?http://localhost:8080',
-      path.join(__dirname, 'src', 'bootstrap'),
-      path.join(__dirname, 'src', 'style.scss')
-    ],
+    main: entryPoints,
     vendor: [
       'react',
       'react-dom',
+      'react-redux',
+      'redux',
+      'react-codemirror',
       require.resolve('@teachers/tpt-connect')
     ]
   },
@@ -33,21 +67,14 @@ module.exports = {
       { test: require.resolve('react'), loader: 'expose?React' },
       { test: require.resolve('react-dom'), loader: 'expose?ReactDOM' },
       { test: require.resolve('@teachers/tpt-connect'), loader: 'expose?Refry' }
+      // uglify everything before so we can analyze size appropriately
+      // { test: /\.jsx?$/, loader: 'uglify' }
     ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx', '.json']
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: path.join(__dirname, 'src', 'index.html')
-    }),
-    new webpack.ResolverPlugin([
-      new ComponentResolverPlugin()
-    ]),
-    new ExtractTextPlugin('style.css', { allChunks: true })
-  ],
+  plugins,
   node: {
     fs: 'empty',
     module: 'empty',
